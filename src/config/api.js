88a6a18ed99e49fb20b18b69f9827e1764dee413
Javith06 <miistahjavith@gmail.com>
2026-04-24 16:51:54 +0530

    Chore: Update API BASE_URL to Railway production URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 //  export const BASE_URL = "https://backoffice-production-fc6a.up.railway.app";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://kds-pos-production.up.railway.app";
  
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
 
